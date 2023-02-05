955506c9f16380af32dfc64560488b3ae6dd4eaa
Massimo Rosin <massimo.rosin@yahoo.de>
2023-02-05 22:38:43 +0100

    initial commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  trailingComma: 'es5',
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  arrowParens: 'always',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  endOfLine: 'crlf',
+};
