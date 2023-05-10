8fd12420408469853b76e07e9c6ca21c007c1fe3
streams-in-silence <massimo.rosin@yahoo.de>
2023-05-10 23:32:58 +0200

    configure dts plugin to break build if type-check fails

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
@@ -10,5 +10,20 @@ export default defineConfig({
       fileName: 'type-guard',
     },
   },
-  plugins: [dts()],
-});
+  plugins: [
+    dts({
+      noEmitOnError: false,
+      afterDiagnostic: (diagnostics) => {
+        if (mode === 'development') {
+          return;
+        }
+        if (
+          (mode === 'production' || mode === 'test') &&
+          diagnostics.length > 0
+        ) {
+          process.exit(1);
+        }
+      },
+    }),
+  ],
+}));
